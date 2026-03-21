144d0a06b464e00e9f46d41229d3f7e482dcbc0a
lianhaibo <957377783@qq.com>
2026-03-21 10:39:16 +0800

    feat: 添加 ArtDictTag 组件并注册为全局组件

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 import type { App } from 'vue'
+import ArtDictTag from '@/components/core/display/art-dict-tag/index.vue'
 import ArtDictCheckboxGroup from '@/components/core/forms/art-dict-checkbox-group/index.vue'
 import ArtDictRadioGroup from '@/components/core/forms/art-dict-radio-group/index.vue'
 import ArtDictSelect from '@/components/core/forms/art-dict-select/index.vue'
@@ -7,6 +8,7 @@ import ArtDictSelect from '@/components/core/forms/art-dict-select/index.vue'
  * 注册业务级全局组件
  */
 export const setupGlobComponents = (app: App<Element>) => {
+  app.component('ArtDictTag', ArtDictTag)
   app.component('ArtDictCheckboxGroup', ArtDictCheckboxGroup)
   app.component('ArtDictRadioGroup', ArtDictRadioGroup)
   app.component('ArtDictSelect', ArtDictSelect)
